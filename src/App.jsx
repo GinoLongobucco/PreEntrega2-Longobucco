0b0aa50ff227044228a2930b1e9939aee5fbf3e0
jorgeduje <jorgeduje4@gmail.com>
2023-06-13 20:50:14 -0300

    se trabajo con jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 
+import { Navbar } from "./Navbar"
+import Footer from "./Footer"
+import { Home } from "./Home";
+
+
 function App() {
 
   return (
     <div>
+      <Navbar />
       <h1>Este es el app</h1>
+      <Footer />
+      <Home />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// Navbar()
+// <Navbar />
+
+
+// const sumar = ()=>{
+
+//   return "hola como estas"
+// }
+
+// sumar() // ---> "hola como estas"
